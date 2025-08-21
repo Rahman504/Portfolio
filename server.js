5721a8327eb4743c86e00b539d424ffdfb10f845
unknown <abdulrahmanabdulazeez34@gmail.com>
2025-08-21 13:51:48 +0100

    portfolio completed

--- server.js
+++ server.js
@@ -40,13 +40,7 @@ app.post("/send", async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

3a8b405ef102e729098d2615cb26dea94d87c4c2
unknown <abdulrahmanabdulazeez34@gmail.com>
2025-10-24 12:53:30 +0100

    portfolio completed

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
-  origin:["https://portfolio-nine-nu-abdulrahman.vercel.app", 
-  "http://localhost:3000"
+  origin:["https://portfolio-nine-nu-abdulrahman.vercel.app"
 ],
   methods: ["POST"],
   allowedHeaders: ["Content-Type"]
